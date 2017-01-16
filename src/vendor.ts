2291d4338b1d47cbe261b352aa03240c1164c2a0
jderose9 <coder@derose.io>
2017-01-16 17:47:14 -0500

    swap es6-shim  and es7-shim for core-js

--- src/vendor.ts
+++ src/vendor.ts
@@ -1,8 +1,4 @@
-// Polyfills
-import 'es6-shim';
-// (these modules are what are in 'angular2/bundles/angular2-polyfills' so don't use that here)
-import 'es6-promise';
-import 'es7-reflect-metadata';
+import 'core-js/client/shim';
 import 'zone.js/dist/zone';
 import 'zone.js/dist/long-stack-trace-zone';
 
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,4 @@
-/// <reference path="globals/es6-shim/index.d.ts" />
+/// <reference path="globals/core-js/index.d.ts" />
 /// <reference path="globals/foundation-sites/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="globals/numeraljs/index.d.ts" />
